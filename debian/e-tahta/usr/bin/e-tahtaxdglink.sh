572b89e79d22354ab6d8860bd097fd5870845c38
bayram <bayramk@gmail.com>
2025-09-21 18:21:35 +0300

    test

---
+++ debian/e-tahta/usr/bin/e-tahtaxdglink.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+FILE="$HOME/Masaüstü/e-tahta.desktop"
+INIFILE="$HOME/e-tahta.ini"
+
+# init
+# look for empty dir 
+
+if [ -f $FILE ]; then
+cp /usr/share/applications/e-tahta.desktop $FILE
+chmod 777 $FILE
+chown $USER:$USER $FILE
+     echo "dosya var"
+else
+cp /usr/share/applications/e-tahta.desktop $FILE
+chmod 777 $FILE
+chown $USER:$USER $FILE
+echo "Dosya Kopyalandı.."
+   
+fi
+
+if [ -f $INIFILE ]; then
+     echo "dosya var"
+     cp /usr/share/e-tahta/e-tahta.ini $INIFILE
+     chmod 777 $INIFILE
+     chown $USER:$USER $INIFILE
+else
+cp /usr/share/e-tahta/e-tahta.ini $INIFILE
+chmod 777 $INIFILE
+chown $USER:$USER $INIFILE
+echo "Dosya Kopyalandı.."
+   
+fi
+exit
+
---
+++ moc_mainwindow.cpp
@@ -0,0 +1,451 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.8)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.8. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[86];
+    char stringdata0[1453];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 15), // "pageButtonClick"
+QT_MOC_LITERAL(2, 27, 0), // ""
+QT_MOC_LITERAL(3, 28, 5), // "index"
+QT_MOC_LITERAL(4, 34, 9), // "colorMenu"
+QT_MOC_LITERAL(5, 44, 6), // "QMenu*"
+QT_MOC_LITERAL(6, 51, 9), // "colorType"
+QT_MOC_LITERAL(7, 61, 13), // "pdfLoaderPage"
+QT_MOC_LITERAL(8, 75, 9), // "pageIndex"
+QT_MOC_LITERAL(9, 85, 9), // "zamanlama"
+QT_MOC_LITERAL(10, 95, 13), // "timerCopySlot"
+QT_MOC_LITERAL(11, 109, 8), // "saatslot"
+QT_MOC_LITERAL(12, 118, 14), // "timerGizleSlot"
+QT_MOC_LITERAL(13, 133, 21), // "currentScreenModeSlot"
+QT_MOC_LITERAL(14, 155, 16), // "siyahButtonClick"
+QT_MOC_LITERAL(15, 172, 18), // "kirmiziButtonClick"
+QT_MOC_LITERAL(16, 191, 20), // "kalemRenkButtonClick"
+QT_MOC_LITERAL(17, 212, 15), // "maviButtonClick"
+QT_MOC_LITERAL(18, 228, 16), // "kalemButtonClick"
+QT_MOC_LITERAL(19, 245, 19), // "kalemPopButtonClick"
+QT_MOC_LITERAL(20, 265, 20), // "kalemMenuButtonClick"
+QT_MOC_LITERAL(21, 286, 24), // "fosforluKalemButtonClick"
+QT_MOC_LITERAL(22, 311, 14), // "silButtonClick"
+QT_MOC_LITERAL(23, 326, 17), // "silPopButtonClick"
+QT_MOC_LITERAL(24, 344, 18), // "silMenuButtonClick"
+QT_MOC_LITERAL(25, 363, 18), // "temizleButtonClick"
+QT_MOC_LITERAL(26, 382, 22), // "temizleMenuButtonClick"
+QT_MOC_LITERAL(27, 405, 16), // "ekranButtonClick"
+QT_MOC_LITERAL(28, 422, 21), // "kalemKapatButtonClick"
+QT_MOC_LITERAL(29, 444, 16), // "sekilButtonClick"
+QT_MOC_LITERAL(30, 461, 19), // "sekilPopButtonClick"
+QT_MOC_LITERAL(31, 481, 23), // "zoomfitSayfaButtonClick"
+QT_MOC_LITERAL(32, 505, 27), // "zoompozitifSayfaButtonClick"
+QT_MOC_LITERAL(33, 533, 27), // "zoomnegatifSayfaButtonClick"
+QT_MOC_LITERAL(34, 561, 16), // "zeminButtonClick"
+QT_MOC_LITERAL(35, 578, 19), // "zeminPopButtonClick"
+QT_MOC_LITERAL(36, 598, 17), // "geriAlButtonClick"
+QT_MOC_LITERAL(37, 616, 14), // "sayfaListeIcon"
+QT_MOC_LITERAL(38, 631, 21), // "ileriSayfaButtonClick"
+QT_MOC_LITERAL(39, 653, 20), // "geriSayfaButtonClick"
+QT_MOC_LITERAL(40, 674, 19), // "silSayfaButtonClick"
+QT_MOC_LITERAL(41, 694, 20), // "ekleSayfaButtonClick"
+QT_MOC_LITERAL(42, 715, 10), // "inserIndex"
+QT_MOC_LITERAL(43, 726, 14), // "pdfObjectAdded"
+QT_MOC_LITERAL(44, 741, 12), // "pdfPageIndex"
+QT_MOC_LITERAL(45, 754, 22), // "screenClickButtonClick"
+QT_MOC_LITERAL(46, 777, 18), // "pagePopButtonClick"
+QT_MOC_LITERAL(47, 796, 25), // "sekilZeminRenkButtonClick"
+QT_MOC_LITERAL(48, 822, 25), // "sekilKalemRenkButtonClick"
+QT_MOC_LITERAL(49, 848, 22), // "zeminSeffafButtonClick"
+QT_MOC_LITERAL(50, 871, 21), // "zeminBeyazButtonClick"
+QT_MOC_LITERAL(51, 893, 21), // "zeminSiyahButtonClick"
+QT_MOC_LITERAL(52, 915, 27), // "zeminCustomColorButtonClick"
+QT_MOC_LITERAL(53, 943, 31), // "zeminCustomColorInitButtonClick"
+QT_MOC_LITERAL(54, 975, 22), // "gridDisableButtonClick"
+QT_MOC_LITERAL(55, 998, 20), // "gridYatayButtonClick"
+QT_MOC_LITERAL(56, 1019, 6), // "toggle"
+QT_MOC_LITERAL(57, 1026, 20), // "gridDikeyButtonClick"
+QT_MOC_LITERAL(58, 1047, 22), // "gizleGosterButtonClick"
+QT_MOC_LITERAL(59, 1070, 14), // "secButtonClick"
+QT_MOC_LITERAL(60, 1085, 15), // "tasiButtonClick"
+QT_MOC_LITERAL(61, 1101, 10), // "setPenSize"
+QT_MOC_LITERAL(62, 1112, 4), // "size"
+QT_MOC_LITERAL(63, 1117, 11), // "setGridSize"
+QT_MOC_LITERAL(64, 1129, 15), // "setSekilPenSize"
+QT_MOC_LITERAL(65, 1145, 16), // "buttonColorClear"
+QT_MOC_LITERAL(66, 1162, 12), // "setEraseSize"
+QT_MOC_LITERAL(67, 1175, 11), // "setPenAlpha"
+QT_MOC_LITERAL(68, 1187, 11), // "setPenStyle"
+QT_MOC_LITERAL(69, 1199, 12), // "Qt::PenStyle"
+QT_MOC_LITERAL(70, 1212, 4), // "styl"
+QT_MOC_LITERAL(71, 1217, 16), // "setSekilPenStyle"
+QT_MOC_LITERAL(72, 1234, 12), // "setSekilType"
+QT_MOC_LITERAL(73, 1247, 24), // "DiagramItem::DiagramType"
+QT_MOC_LITERAL(74, 1272, 3), // "typ"
+QT_MOC_LITERAL(75, 1276, 15), // "infoButtonClick"
+QT_MOC_LITERAL(76, 1292, 17), // "klavyeButtonClick"
+QT_MOC_LITERAL(77, 1310, 15), // "saveButtonClick"
+QT_MOC_LITERAL(78, 1326, 15), // "openButtonClick"
+QT_MOC_LITERAL(79, 1342, 11), // "kalemEgitim"
+QT_MOC_LITERAL(80, 1354, 21), // "zeminMuzikButtonClick"
+QT_MOC_LITERAL(81, 1376, 25), // "zeminGuzelYaziButtonClick"
+QT_MOC_LITERAL(82, 1402, 19), // "sekilButtonIconSlot"
+QT_MOC_LITERAL(83, 1422, 7), // "initPen"
+QT_MOC_LITERAL(84, 1430, 11), // "kalemLayout"
+QT_MOC_LITERAL(85, 1442, 10) // "resizeSlot"
+
+    },
+    "MainWindow\0pageButtonClick\0\0index\0"
+    "colorMenu\0QMenu*\0colorType\0pdfLoaderPage\0"
+    "pageIndex\0zamanlama\0timerCopySlot\0"
+    "saatslot\0timerGizleSlot\0currentScreenModeSlot\0"
+    "siyahButtonClick\0kirmiziButtonClick\0"
+    "kalemRenkButtonClick\0maviButtonClick\0"
+    "kalemButtonClick\0kalemPopButtonClick\0"
+    "kalemMenuButtonClick\0fosforluKalemButtonClick\0"
+    "silButtonClick\0silPopButtonClick\0"
+    "silMenuButtonClick\0temizleButtonClick\0"
+    "temizleMenuButtonClick\0ekranButtonClick\0"
+    "kalemKapatButtonClick\0sekilButtonClick\0"
+    "sekilPopButtonClick\0zoomfitSayfaButtonClick\0"
+    "zoompozitifSayfaButtonClick\0"
+    "zoomnegatifSayfaButtonClick\0"
+    "zeminButtonClick\0zeminPopButtonClick\0"
+    "geriAlButtonClick\0sayfaListeIcon\0"
+    "ileriSayfaButtonClick\0geriSayfaButtonClick\0"
+    "silSayfaButtonClick\0ekleSayfaButtonClick\0"
+    "inserIndex\0pdfObjectAdded\0pdfPageIndex\0"
+    "screenClickButtonClick\0pagePopButtonClick\0"
+    "sekilZeminRenkButtonClick\0"
+    "sekilKalemRenkButtonClick\0"
+    "zeminSeffafButtonClick\0zeminBeyazButtonClick\0"
+    "zeminSiyahButtonClick\0zeminCustomColorButtonClick\0"
+    "zeminCustomColorInitButtonClick\0"
+    "gridDisableButtonClick\0gridYatayButtonClick\0"
+    "toggle\0gridDikeyButtonClick\0"
+    "gizleGosterButtonClick\0secButtonClick\0"
+    "tasiButtonClick\0setPenSize\0size\0"
+    "setGridSize\0setSekilPenSize\0"
+    "buttonColorClear\0setEraseSize\0setPenAlpha\0"
+    "setPenStyle\0Qt::PenStyle\0styl\0"
+    "setSekilPenStyle\0setSekilType\0"
+    "DiagramItem::DiagramType\0typ\0"
+    "infoButtonClick\0klavyeButtonClick\0"
+    "saveButtonClick\0openButtonClick\0"
+    "kalemEgitim\0zeminMuzikButtonClick\0"
+    "zeminGuzelYaziButtonClick\0sekilButtonIconSlot\0"
+    "initPen\0kalemLayout\0resizeSlot"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      71,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,  369,    2, 0x08 /* Private */,
+       4,    1,  372,    2, 0x08 /* Private */,
+       7,    1,  375,    2, 0x08 /* Private */,
+       9,    0,  378,    2, 0x08 /* Private */,
+      10,    0,  379,    2, 0x08 /* Private */,
+      11,    0,  380,    2, 0x08 /* Private */,
+      12,    0,  381,    2, 0x08 /* Private */,
+      13,    0,  382,    2, 0x08 /* Private */,
+      14,    0,  383,    2, 0x08 /* Private */,
+      15,    0,  384,    2, 0x08 /* Private */,
+      16,    0,  385,    2, 0x08 /* Private */,
+      17,    0,  386,    2, 0x08 /* Private */,
+      18,    0,  387,    2, 0x08 /* Private */,
+      19,    0,  388,    2, 0x08 /* Private */,
+      20,    0,  389,    2, 0x08 /* Private */,
+      21,    0,  390,    2, 0x08 /* Private */,
+      22,    0,  391,    2, 0x08 /* Private */,
+      23,    0,  392,    2, 0x08 /* Private */,
+      24,    0,  393,    2, 0x08 /* Private */,
+      25,    0,  394,    2, 0x08 /* Private */,
+      26,    0,  395,    2, 0x08 /* Private */,
+      27,    0,  396,    2, 0x08 /* Private */,
+      28,    0,  397,    2, 0x08 /* Private */,
+      29,    0,  398,    2, 0x08 /* Private */,
+      30,    0,  399,    2, 0x08 /* Private */,
+      31,    0,  400,    2, 0x08 /* Private */,
+      32,    0,  401,    2, 0x08 /* Private */,
+      33,    0,  402,    2, 0x08 /* Private */,
+      34,    0,  403,    2, 0x08 /* Private */,
+      35,    0,  404,    2, 0x08 /* Private */,
+      36,    0,  405,    2, 0x08 /* Private */,
+      37,    0,  406,    2, 0x08 /* Private */,
+      38,    0,  407,    2, 0x08 /* Private */,
+      39,    0,  408,    2, 0x08 /* Private */,
+      40,    0,  409,    2, 0x08 /* Private */,
+      41,    3,  410,    2, 0x08 /* Private */,
+      45,    0,  417,    2, 0x08 /* Private */,
+      46,    0,  418,    2, 0x08 /* Private */,
+      47,    0,  419,    2, 0x08 /* Private */,
+      48,    0,  420,    2, 0x08 /* Private */,
+      49,    0,  421,    2, 0x08 /* Private */,
+      50,    0,  422,    2, 0x08 /* Private */,
+      51,    0,  423,    2, 0x08 /* Private */,
+      52,    0,  424,    2, 0x08 /* Private */,
+      53,    0,  425,    2, 0x08 /* Private */,
+      54,    0,  426,    2, 0x08 /* Private */,
+      55,    1,  427,    2, 0x08 /* Private */,
+      57,    1,  430,    2, 0x08 /* Private */,
+      58,    0,  433,    2, 0x08 /* Private */,
+      59,    0,  434,    2, 0x08 /* Private */,
+      60,    0,  435,    2, 0x08 /* Private */,
+      61,    1,  436,    2, 0x08 /* Private */,
+      63,    1,  439,    2, 0x08 /* Private */,
+      64,    1,  442,    2, 0x08 /* Private */,
+      65,    0,  445,    2, 0x08 /* Private */,
+      66,    1,  446,    2, 0x08 /* Private */,
+      67,    1,  449,    2, 0x08 /* Private */,
+      68,    1,  452,    2, 0x08 /* Private */,
+      71,    1,  455,    2, 0x08 /* Private */,
+      72,    1,  458,    2, 0x08 /* Private */,
+      75,    0,  461,    2, 0x08 /* Private */,
+      76,    0,  462,    2, 0x08 /* Private */,
+      77,    0,  463,    2, 0x08 /* Private */,
+      78,    0,  464,    2, 0x08 /* Private */,
+      79,    0,  465,    2, 0x08 /* Private */,
+      80,    0,  466,    2, 0x08 /* Private */,
+      81,    1,  467,    2, 0x08 /* Private */,
+      82,    0,  470,    2, 0x08 /* Private */,
+      83,    0,  471,    2, 0x08 /* Private */,
+      84,    0,  472,    2, 0x08 /* Private */,
+      85,    0,  473,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int,    3,
+    0x80000000 | 5, QMetaType::QString,    6,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Bool, QMetaType::Int,   42,   43,   44,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   56,
+    QMetaType::Void, QMetaType::Bool,   56,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   62,
+    QMetaType::Void, QMetaType::Int,   62,
+    QMetaType::Void, QMetaType::Int,   62,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   62,
+    QMetaType::Void, QMetaType::Int,   62,
+    QMetaType::Void, 0x80000000 | 69,   70,
+    QMetaType::Void, 0x80000000 | 69,   70,
+    QMetaType::Void, 0x80000000 | 73,   74,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   56,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->pageButtonClick((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 1: { QMenu* _r = _t->colorMenu((*reinterpret_cast< QString(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< QMenu**>(_a[0]) = std::move(_r); }  break;
+        case 2: _t->pdfLoaderPage((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->zamanlama(); break;
+        case 4: _t->timerCopySlot(); break;
+        case 5: _t->saatslot(); break;
+        case 6: _t->timerGizleSlot(); break;
+        case 7: _t->currentScreenModeSlot(); break;
+        case 8: _t->siyahButtonClick(); break;
+        case 9: _t->kirmiziButtonClick(); break;
+        case 10: _t->kalemRenkButtonClick(); break;
+        case 11: _t->maviButtonClick(); break;
+        case 12: _t->kalemButtonClick(); break;
+        case 13: _t->kalemPopButtonClick(); break;
+        case 14: _t->kalemMenuButtonClick(); break;
+        case 15: _t->fosforluKalemButtonClick(); break;
+        case 16: _t->silButtonClick(); break;
+        case 17: _t->silPopButtonClick(); break;
+        case 18: _t->silMenuButtonClick(); break;
+        case 19: _t->temizleButtonClick(); break;
+        case 20: _t->temizleMenuButtonClick(); break;
+        case 21: _t->ekranButtonClick(); break;
+        case 22: _t->kalemKapatButtonClick(); break;
+        case 23: _t->sekilButtonClick(); break;
+        case 24: _t->sekilPopButtonClick(); break;
+        case 25: _t->zoomfitSayfaButtonClick(); break;
+        case 26: _t->zoompozitifSayfaButtonClick(); break;
+        case 27: _t->zoomnegatifSayfaButtonClick(); break;
+        case 28: _t->zeminButtonClick(); break;
+        case 29: _t->zeminPopButtonClick(); break;
+        case 30: _t->geriAlButtonClick(); break;
+        case 31: _t->sayfaListeIcon(); break;
+        case 32: _t->ileriSayfaButtonClick(); break;
+        case 33: _t->geriSayfaButtonClick(); break;
+        case 34: _t->silSayfaButtonClick(); break;
+        case 35: _t->ekleSayfaButtonClick((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3]))); break;
+        case 36: _t->screenClickButtonClick(); break;
+        case 37: _t->pagePopButtonClick(); break;
+        case 38: _t->sekilZeminRenkButtonClick(); break;
+        case 39: _t->sekilKalemRenkButtonClick(); break;
+        case 40: _t->zeminSeffafButtonClick(); break;
+        case 41: _t->zeminBeyazButtonClick(); break;
+        case 42: _t->zeminSiyahButtonClick(); break;
+        case 43: _t->zeminCustomColorButtonClick(); break;
+        case 44: _t->zeminCustomColorInitButtonClick(); break;
+        case 45: _t->gridDisableButtonClick(); break;
+        case 46: _t->gridYatayButtonClick((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 47: _t->gridDikeyButtonClick((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 48: _t->gizleGosterButtonClick(); break;
+        case 49: _t->secButtonClick(); break;
+        case 50: _t->tasiButtonClick(); break;
+        case 51: _t->setPenSize((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 52: _t->setGridSize((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 53: _t->setSekilPenSize((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 54: _t->buttonColorClear(); break;
+        case 55: _t->setEraseSize((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 56: _t->setPenAlpha((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 57: _t->setPenStyle((*reinterpret_cast< const Qt::PenStyle(*)>(_a[1]))); break;
+        case 58: _t->setSekilPenStyle((*reinterpret_cast< const Qt::PenStyle(*)>(_a[1]))); break;
+        case 59: _t->setSekilType((*reinterpret_cast< const DiagramItem::DiagramType(*)>(_a[1]))); break;
+        case 60: _t->infoButtonClick(); break;
+        case 61: _t->klavyeButtonClick(); break;
+        case 62: _t->saveButtonClick(); break;
+        case 63: _t->openButtonClick(); break;
+        case 64: _t->kalemEgitim(); break;
+        case 65: _t->zeminMuzikButtonClick(); break;
+        case 66: _t->zeminGuzelYaziButtonClick((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 67: _t->sekilButtonIconSlot(); break;
+        case 68: _t->initPen(); break;
+        case 69: _t->kalemLayout(); break;
+        case 70: _t->resizeSlot(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 71)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 71;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 71)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 71;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
